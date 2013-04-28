87f98c4954595eacda7a0165d9087662363cf115
whiskers75 <whiskers75@me.com>
2013-04-28 19:21:10 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2024,7 +2024,7 @@ bool LoadBlockIndex(bool fAllowNew)
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x00000000000000000017135cc76e65e8f292020ab2fc92e969e65c145ecf6e85"));
+        assert(block.hashMerkleRoot == uint256("0x17135cc76e65e8f292020ab2fc92e969e65c145ecf6e85"));
 
         // If genesis block hash does not match, then generate new genesis hash.
         if (false && block.GetHash() != hashGenesisBlock)
