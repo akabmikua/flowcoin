3838c52bd1392c2a01a9672fe67fdc6e48aa5e21
whiskers75 <whiskers75@me.com>
2013-04-28 13:47:11 +0200

    Update IRC channels

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #litecoinTEST3\r");
-            Send(hSocket, "WHO #litecoinTEST3\r");
+            Send(hSocket, "JOIN #flowcoinTEST3\r");
+            Send(hSocket, "WHO #flowcoinTEST3\r");
         } else {
-            // randomly join #litecoin00-#litecoin99
+            // randomly join #flowcoin-(00-99)
             int channel_number = GetRandInt(100);
-            channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #litecoin%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #litecoin%02d\r", channel_number).c_str());
+            channel_number = 0; // Flowoin: for now, just use one channel
+            Send(hSocket, strprintf("JOIN #flowcoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #flowcoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
